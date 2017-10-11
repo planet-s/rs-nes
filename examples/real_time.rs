42e0e92465625ce8bf24fb2dbd18b9a13a812db4
Jeremy Soller <jackpot51@gmail.com>
2017-10-10 21:27:10 -0600

    Port to orbclient

--- examples/real_time.rs
+++ examples/real_time.rs
@@ -1,15 +1,13 @@
-extern crate sdl2;
+extern crate orbclient;
 extern crate rs_nes;
 
+use orbclient::{Color, EventOption, Renderer, Window, WindowFlag};
 use rs_nes::cpu::*;
 use rs_nes::input::{Button, Input, InputBase};
 use rs_nes::memory::Memory;
 use rs_nes::memory::nes_memory::NesMemoryImpl;
 use rs_nes::ppu::{Ppu, PpuImpl};
 use rs_nes::rom::NesRom;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use sdl2::pixels::PixelFormatEnum;
 use std::env;
 use std::rc::Rc;
 use std::thread;
@@ -33,66 +31,50 @@ fn main() {
     let mut cpu = Cpu::new(mem);
     cpu.reset();
 
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let mut window = Window::new_flags(
+        0, 0, SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2, "RS-NES!", &[WindowFlag::Async]
+    ).unwrap();
 
-    let window = video_subsystem
-        .window("RS-NES!", SCREEN_WIDTH * 2, SCREEN_HEIGHT * 2)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
+    window.set(Color::rgb(0, 0, 0));
+    window.sync();
 
-    let mut renderer = window
-        .renderer()
-        .accelerated()
-        .present_vsync()
-        .build()
-        .unwrap();
-
-    let mut texture = renderer
-        .create_texture_streaming(PixelFormatEnum::RGB24, SCREEN_WIDTH, SCREEN_HEIGHT)
-        .unwrap();
-
-    let mut event_pump = sdl_context.event_pump().unwrap();
     let mut accumulator = Duration::new(0, 0);
     let mut previous_clock = Instant::now();
 
     'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit { .. } |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
-                Event::KeyDown { keycode: Some(keycode), .. } => {
-                    match keycode {
-                        Keycode::W => cpu.memory.input().player1_press(Button::Up),
-                        Keycode::A => cpu.memory.input().player1_press(Button::Left),
-                        Keycode::S => cpu.memory.input().player1_press(Button::Down),
-                        Keycode::D => cpu.memory.input().player1_press(Button::Right),
-                        Keycode::LShift | Keycode::RShift => {
+        for event in window.events(){
+            match event.to_option() {
+                EventOption::Quit(_) => break 'running,
+                EventOption::Key(key_event) => if key_event.pressed {
+                    match key_event.scancode {
+                        orbclient::K_ESC => break 'running,
+                        orbclient::K_W => cpu.memory.input().player1_press(Button::Up),
+                        orbclient::K_A => cpu.memory.input().player1_press(Button::Left),
+                        orbclient::K_S => cpu.memory.input().player1_press(Button::Down),
+                        orbclient::K_D => cpu.memory.input().player1_press(Button::Right),
+                        orbclient::K_LEFT_SHIFT | orbclient::K_RIGHT_SHIFT => {
                             cpu.memory.input().player1_press(Button::Select)
-                        }
-                        Keycode::Return => cpu.memory.input().player1_press(Button::Start),
-                        Keycode::J => cpu.memory.input().player1_press(Button::B),
-                        Keycode::K => cpu.memory.input().player1_press(Button::A),
+                        },
+                        orbclient::K_ENTER => cpu.memory.input().player1_press(Button::Start),
+                        orbclient::K_J => cpu.memory.input().player1_press(Button::B),
+                        orbclient::K_K => cpu.memory.input().player1_press(Button::A),
                         _ => (),
                     }
-                }
-                Event::KeyUp { keycode: Some(keycode), .. } => {
-                    match keycode {
-                        Keycode::W => cpu.memory.input().player1_release(Button::Up),
-                        Keycode::A => cpu.memory.input().player1_release(Button::Left),
-                        Keycode::S => cpu.memory.input().player1_release(Button::Down),
-                        Keycode::D => cpu.memory.input().player1_release(Button::Right),
-                        Keycode::LShift | Keycode::RShift => {
+                } else {
+                    match key_event.scancode {
+                        orbclient::K_W => cpu.memory.input().player1_release(Button::Up),
+                        orbclient::K_A => cpu.memory.input().player1_release(Button::Left),
+                        orbclient::K_S => cpu.memory.input().player1_release(Button::Down),
+                        orbclient::K_D => cpu.memory.input().player1_release(Button::Right),
+                        orbclient::K_LEFT_SHIFT | orbclient::K_RIGHT_SHIFT => {
                             cpu.memory.input().player1_release(Button::Select)
-                        }
-                        Keycode::Return => cpu.memory.input().player1_release(Button::Start),
-                        Keycode::J => cpu.memory.input().player1_release(Button::B),
-                        Keycode::K => cpu.memory.input().player1_release(Button::A),
+                        },
+                        orbclient::K_ENTER => cpu.memory.input().player1_release(Button::Start),
+                        orbclient::K_J => cpu.memory.input().player1_release(Button::B),
+                        orbclient::K_K => cpu.memory.input().player1_release(Button::A),
                         _ => (),
                     }
-                }
+                },
                 _ => (),
             }
         }
@@ -107,12 +89,26 @@ fn main() {
             loop {
                 if cpu.step() == Interrupt::Nmi {
                     let screen_buffer = &*cpu.memory.screen().screen_buffer;
-                    texture
-                        .update(None, screen_buffer, SCREEN_WIDTH as usize * 3)
-                        .unwrap();
-                    renderer.clear();
-                    renderer.copy(&texture, None, None).unwrap();
-                    renderer.present();
+                    {
+                        let data = window.data_mut();
+                        for y in 0..SCREEN_HEIGHT as usize {
+                            for x in 0..SCREEN_WIDTH as usize {
+                                let i = (y * SCREEN_WIDTH as usize + x) * 3;
+                                let color = Color::rgb(
+                                    screen_buffer[i + 0],
+                                    screen_buffer[i + 1],
+                                    screen_buffer[i + 2]
+                                );
+
+                                let j = y * 2 * SCREEN_WIDTH as usize * 2 + x * 2;
+                                data[j] = color;
+                                data[j + 1] = color;
+                                data[j + SCREEN_WIDTH as usize * 2] = color;
+                                data[j + SCREEN_WIDTH as usize * 2 + 1] = color;
+                            }
+                        }
+                    }
+                    window.sync();
                     break;
                 }
             }
